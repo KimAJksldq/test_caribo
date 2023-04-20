c31e8f0ddcb4a0ba439d0045ae20bf1fdf8e0142
KimAJksldq <wlghks0106@naver.com>
2023-04-20 12:10:50 +0900

    1차 테스트본 완성

---
+++ js/swiper.js
@@ -0,0 +1,42 @@
+const swiperEl = document.querySelector(".swiper-wrapper");
+
+const bannerActive = (index) => {};
+
+const sliderSelector = ".swiper-container",
+  options = {
+    init: false,
+    speed: 1500,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        allowTouchMove: true,
+        // 터치 방지
+      },
+    },
+    // Events
+    on: {
+      imagesReady: function () {
+        this.el.classList.remove("loading");
+      },
+      activeIndexChange: function () {
+        bannerActive(this.realIndex);
+      },
+    },
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  };
+const mySwiper = new Swiper(sliderSelector, options);
+// Initialize slider
+mySwiper.init();
