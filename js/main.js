29382794ea4a702421df39c80d5c25834da29067
kimJiHwan0618 <wlghks0618@kakao.com>
2023-04-25 01:15:04 +0900

    디자인 수정 반영

--- js/main.js
+++ js/main.js
@@ -1,93 +1,108 @@
 $(document).ready(function () {
-    let scrollY = 0;
-    let screenHeight = screen.height;
-    const sectionArray  = [
-      $("#section1"),
-      $("#section2"),
-      $("#section3"),
-      $("#section4"),
-      $("#section5"),
-      $("#section6"),
-    ]
+  let scrollY = 0;
+  let screenHeight = screen.height;
+  const sectionArray = [
+    $('#section1'),
+    $('#section2'),
+    $('#section3'),
+    $('#section4'),
+    $('#section5'),
+    $('#section6'),
+  ];
 
-    $("td-card").click(function(){
-      if($(this).attr("class") === "card-0"){
-        $("#swiperWrap").addClass("on")
-      }
-    })
-
-    $(".close__btn").click(function(){
-      $("#swiperWrap").removeClass("on")
-    })
-
-    $("nav ul>li>.icon").click(function () {
-      const idx = $(this).parent().index();
-      if(scrollY < idx) {
-        $('#main').animate({scrollTop : sectionArray[idx][0].offsetTop + 10 }, 400);
-      } else {
-        $('#main').animate({scrollTop : sectionArray[idx][0].offsetTop - 10  }, 400);
-        $('#main').animate({scrollTop : sectionArray[idx][0].offsetTop + 0.0001 }, 10);
-      }
-    });
-
-    let lastScrollY = 0;
-    $("#main.scroll").scroll(function(){
-        const lScrollY = $("#main").scrollTop();
-        // 이전의 스크롤 위치와 비교하기
-        const direction = lScrollY > lastScrollY ? "Scroll Down" : "Scroll Up";
-         // 현재의 스크롤 값을 저장
-        lastScrollY = lScrollY;
-        if(direction === "Scroll Down"){
-          for(let i = 0 ; i < sectionArray.length ; i ++) {
-            if(sectionArray[i][0].offsetTop - 20 < $("#main").scrollTop()) {
-              if(scrollY < i) {
-                scrollY = i;
-                activeSection(i)
-              } 
-            } else {
-              if($("#main").scrollTop() === 0) {
-                scrollY = 0
-                activeSection(0)
-              }
-              if(scrollY > i) {
-                scrollY = i
-                activeSection(i)
-              } 
-            }
-          } 
-        } else if (direction === "Scroll Up") {
-          for(let i = 0 ; i < sectionArray.length ; i ++) {
-            if(sectionArray[i][0].offsetTop + $(sectionArray[i][0]).height() / 2 < $("#main").scrollTop()) {
-              if(scrollY < i) {
-                scrollY = i;
-                activeSection(i)
-              } 
-            } else {
-              if($("#main").scrollTop() === 0) {
-                scrollY = 0
-                activeSection(0)
-              }
-              if(scrollY > i) {
-                scrollY = i
-                activeSection(i)
-              } 
-            }
-          } 
-        }
-    })
-
-    const activeSection = (idx) => {
-      $("nav ul>li").removeClass("on")
-      $("#main>.inner>section").removeClass("on")
-      $("nav ul>li").eq(idx).addClass("on")
-      $("#main>.inner>section").eq(idx).addClass("on")
+  $('td-card').click(function () {
+    if ($(this).attr('class') === 'card-0') {
+      $('#swiperWrap .mySwiper').remove();
+      $('#swiperWrap').append(`
+        <section class="mySwiper swiper-container"></section>
+      `);
+      $('#galleryImgWrap')
+        .children()
+        .eq($(this).attr('data-index'))
+        .clone()
+        .addClass('swiper-wrapper')
+        .appendTo('.mySwiper.swiper-container');
+      $('#swiperWrap').addClass('on');
+      const mySwiper = new Swiper('.swiper-container', options);
+      mySwiper.init();
     }
-
-    $(window).resize(function(){
-      if(screenHeight < $(window).height()){
-        screenHeight = $(window).height()
-        $("#main.scroll").smoothWheel();
-      } 
-    })
-    $("#main.scroll").smoothWheel();
   });
+
+  $('.close__btn').click(function () {
+    $('#swiperWrap').removeClass('on');
+  });
+
+  $('nav ul>li>.icon').click(function () {
+    const idx = $(this).parent().index();
+    if (scrollY < idx) {
+      $('#main').animate({ scrollTop: sectionArray[idx][0].offsetTop + 10 }, 400);
+    } else {
+      $('#main').animate({ scrollTop: sectionArray[idx][0].offsetTop - 10 }, 400);
+      $('#main').animate({ scrollTop: sectionArray[idx][0].offsetTop + 0.0001 }, 10);
+    }
+  });
+
+  let lastScrollY = 0;
+  $('#main.scroll').scroll(function () {
+    const lScrollY = $('#main').scrollTop();
+    // 이전의 스크롤 위치와 비교하기
+    const direction = lScrollY > lastScrollY ? 'Scroll Down' : 'Scroll Up';
+    // 현재의 스크롤 값을 저장
+    lastScrollY = lScrollY;
+    if (direction === 'Scroll Down') {
+      for (let i = 0; i < sectionArray.length; i++) {
+        if (sectionArray[i][0].offsetTop - 20 < $('#main').scrollTop()) {
+          if (scrollY < i) {
+            scrollY = i;
+            activeSection(i);
+          }
+        } else {
+          if ($('#main').scrollTop() === 0) {
+            scrollY = 0;
+            activeSection(0);
+          }
+          if (scrollY > i) {
+            scrollY = i;
+            activeSection(i);
+          }
+        }
+      }
+    } else if (direction === 'Scroll Up') {
+      for (let i = 0; i < sectionArray.length; i++) {
+        if (
+          sectionArray[i][0].offsetTop + $(sectionArray[i][0]).height() / 2 <
+          $('#main').scrollTop()
+        ) {
+          if (scrollY < i) {
+            scrollY = i;
+            activeSection(i);
+          }
+        } else {
+          if ($('#main').scrollTop() === 0) {
+            scrollY = 0;
+            activeSection(0);
+          }
+          if (scrollY > i) {
+            scrollY = i;
+            activeSection(i);
+          }
+        }
+      }
+    }
+  });
+
+  const activeSection = (idx) => {
+    $('nav ul>li').removeClass('on');
+    $('#main>.inner>section').removeClass('on');
+    $('nav ul>li').eq(idx).addClass('on');
+    $('#main>.inner>section').eq(idx).addClass('on');
+  };
+
+  $(window).resize(function () {
+    if (screenHeight < $(window).height()) {
+      screenHeight = $(window).height();
+      $('#main.scroll').smoothWheel();
+    }
+  });
+  $('#main.scroll').smoothWheel();
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,42 +1,38 @@
-const swiperEl = document.querySelector(".swiper-wrapper");
+const swiperEl = document.querySelector('.swiper-wrapper');
 
 const bannerActive = (index) => {};
 
-const sliderSelector = ".swiper-container",
-  options = {
-    init: false,
-    speed: 1500,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+const options = {
+  init: false,
+  speed: 1500,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      allowTouchMove: true,
+      // 터치 방지
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  },
+  // Events
+  on: {
+    imagesReady: function () {
+      this.el.classList.remove('loading');
     },
-    breakpoints: {
-      350: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        allowTouchMove: true,
-        // 터치 방지
-      },
+    activeIndexChange: function () {
+      bannerActive(this.realIndex);
     },
-    // Events
-    on: {
-      imagesReady: function () {
-        this.el.classList.remove("loading");
-      },
-      activeIndexChange: function () {
-        bannerActive(this.realIndex);
-      },
-    },
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-  };
-const mySwiper = new Swiper(sliderSelector, options);
-// Initialize slider
-mySwiper.init();
+  },
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+};
