4d930b33ac0af7b9fc5740d8d10f4195da489d97
KimAJksldq <wlghks0106@naver.com>
2023-05-03 16:33:02 +0900

    contact-us 화면 뷰, 이메일 기능 테스트 추가

--- js/main.js
+++ js/main.js
@@ -1,35 +1,106 @@
 $(document).ready(function () {
-
-  const form = document.getElementById('myForm');
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    $("#loadingWrap").removeClass("open")
-    const formData = new FormData(form);
-    const url = 'https://script.google.com/macros/s/AKfycbz48R-IX5THFW55qPG4E5OOT-U6EOX77Iq8ObByPqlt_SpOcu0EuvbU5oaTmC73ifmK/exec';
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: formData
-      });
-      if (response.ok) {
-        $("#emailInput").val("");
-        $("#titleInput").val("");
-        $("#msgInput").val("");
-        $("#phoneInput").val("");
-        alert('메시지가 전송되었습니다!');
-      } else {
-        alert('메시지 전송에 실패했습니다.');
-      }
-      $("#loadingWrap").addClass("open")
-    } catch (error) {
-      alert('메시지 전송에 실패했습니다.');
-      $("#loadingWrap").addClass("open")
-      console.error(error);
-    }
-  });
   
+  let timerId = null;
   let scrollY = 0;
   let screenHeight = screen.height;
+
+  const msgCardActive = () => {
+    clearTimeout(timerId);
+    $('#messageCard').addClass('on');
+    timerId = setTimeout(function () {
+      $('#messageCard').removeClass('on');
+    }, 3000);
+  }
+
+  const form = document.getElementById('contactForm');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const v1 = $("#subjectInput").val();
+    const v2 = $("#titleInput").val();
+    const v3 = $("#emailInput").val();
+    const v4 = $("#phoneInput").val();
+    const v5 = $("#bodyInput").val();
+    if(v2 === ""){
+      $('#messageCard p').text("제목을 입력해주세요.");
+      msgCardActive();
+      return;
+    }
+    if(v3 === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v3)){
+      $('#messageCard p').text("이메을을 확인해주세요.");
+      msgCardActive();
+      return;
+    }
+    if(v4 === "" || !/^[0-9]{9,11}$/i.test(v4)){
+      $('#messageCard p').text("번호를 확인해주세요.");
+      msgCardActive();
+      return;
+    }
+    if(v5 === ""){
+      $('#messageCard p').text("내용을 입력해주세요.");
+      msgCardActive();
+      return;
+    }
+    $(".loadingWrap.email").removeClass("open");
+    const param = {
+      pw : "WlGhks010!@#",
+      from : v3,
+      to : "nnkstory@naver.com",
+      subject : `[${v1}] ${v2}`,
+      title : v2,
+      body : `
+      <div style="margin: 16px 0 0 0; padding: 12px 12px 60px 12px;box-sizing: border-box;">
+        <div style="padding: 34px; box-sizing: border-box;background: #fff;border-radius: 12px;box-shadow: 0 0.75rem 2rem 0 rgba(0, 0, 0, 0.1);">
+          <h1 style="margin: 0;padding: 0; color: #414141;">'OOO'님의 사이트로부터 문의 메일이 도착했습니다.</h1>
+          <p style="color: #999; margin: 8px 0 34px 0; padding: 0;">메일 내용은 아래와 같습니다.</p>
+      
+          <ul style="list-style: none;margin: 0 0 38px 0;padding: 0 0 18px 0; border-bottom: 1px solid rgb(231, 231, 231);">
+            <li style="margin-bottom: 12px;">
+              <dl style="display: flex;align-items: flex-start;">
+                <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">제목&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7">[${v1}] ${v2}</dd>
+              </dl>  
+            </li>
+            <li style="margin-bottom: 12px;">
+              <dl style="display: flex;align-items: flex-start;">
+                <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">이메일&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7;">${v3}</dd>
+              </dl>  
+            </li>
+            <li>
+              <dl style="display: flex;align-items: flex-start;">
+                <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">연락처&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7;">${v4}</dd>
+              </dl>  
+            </li>
+          </ul>
+          <p style="margin-top:30px;">${v5}</p>
+        </div>
+      </div>
+      `
+    }
+
+    try {
+      const res = await fetch ('https://port-0-email-service-687p2alh73yb2l.sel4.cloudtype.app/send', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(param),
+      })
+      if(res.ok) {
+        $("#titleInput").val("");
+        $("#emailInput").val("");
+        $("#phoneInput").val("");
+        $("#bodyInput").val("");
+        alert("메일 전송되었습니다!");
+      } else {
+        alert("메일 전송이 실패했습니다.");
+      };
+      $(".loadingWrap.email").addClass("open");
+    } catch (error) {
+      alert("메일 전송이 실패했습니다.");
+      $(".loadingWrap.email").addClass("open");
+    }
+
+  })
   
   const sectionArray = [
     $('#section1'),
@@ -39,22 +110,14 @@ $(document).ready(function () {
     $('#section5'),
     $('#section6'),
     $('#section7'),
+    $('#section8'),
   ];
 
-  let idtest = null;
-
-  // function hideMessage() {
-  //   $('#messageCard').removeClass('on');
-  // }
-
   $('#phoneCopyBtn').click(function () {
+    $('#messageCard p').text("클립보드에 복사되었습니다.")
     navigator.clipboard.writeText('01027251185').then(
       () => {
-        clearTimeout(idtest);
-        $('#messageCard').addClass('on');
-        idtest = setTimeout(function () {
-          $('#messageCard').removeClass('on');
-        }, 3000);
+        msgCardActive();
       },
       (error) => {
         console.error('Error copying text: ', error);
@@ -62,29 +125,16 @@ $(document).ready(function () {
     );
   });
   $('#mailCopyBtn').click(function () {
+    $('#messageCard p').text("클립보드에 복사되었습니다.")
     navigator.clipboard.writeText('nnkstory@naver.com').then(
       () => {
-        clearTimeout(idtest);
-        $('#messageCard').addClass('on');
-        idtest = setTimeout(function () {
-          $('#messageCard').removeClass('on');
-        }, 3000);
+        msgCardActive();
       },
       (error) => {
         console.error('Error copying text: ', error);
       }
     );
   });
-
-  // window.navigator.clipboard.writeText('테스트 복사').then(
-  //   () => {
-  //     // 복사가 완료되면 이 부분이 호출된다.
-  //     alert('복사 완료!');
-  //   },
-  //   (error) => {
-  //     console.error('Error copying text: ', error);
-  //   }
-  // );
 
   $('td-card').click(function () {
     if ($(this).attr('class') === 'card-0') {
@@ -146,8 +196,7 @@ $(document).ready(function () {
     } else if (direction === 'Scroll Up') {
       for (let i = 0; i < sectionArray.length; i++) {
         if (
-          sectionArray[i][0].offsetTop + $(sectionArray[i][0]).height() / 2 <
-          $('#main').scrollTop()
+          sectionArray[i][0].offsetTop + $(sectionArray[i][0]).height() / 2 < $('#main').scrollTop()
         ) {
           if (scrollY < i) {
             scrollY = i;
